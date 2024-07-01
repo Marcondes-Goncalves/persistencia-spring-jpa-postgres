54bbd24fa21a16cddbcad81ae635be7c59eba09e
Marcondes <marcondesgoncalves159@gmail.com>
2024-07-01 20:30:51 -0300

    Projeto Criado

---
+++ src/main/java/com/postgres/aprender/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.postgres.aprender;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AprenderApplication.class);
+	}
+
+}
